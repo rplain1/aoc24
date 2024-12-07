25303bbbd58439a17b035e9ab3fa90d632b88cb1
rplain1 <ryan.plain1@gmail.com>
2024-12-07 09:00:37 -0700

    document code

--- R/day01.R
+++ R/day01.R
@@ -1,39 +1,88 @@
-library(data.table)
+#' Day 01 Solutions: Historian Hysteria
+#'
+#' Functions to solve Day 01 puzzles.
+#'
+#' @section Base R Functions:
+#' - `base_01_input()`: Reads the input file.
+#' - `base_01_distance()`: Computes the total distance.
+#' - `base_01_similarity()`: Computes the similarity score.
+#'
+#' @section data.table Functions:
+#' - `dt_01_input()`: Reads the input file using data.table.
+#' - `dt_01_distance()`: Computes the total distance using data.table.
+#' - `dt_01_similarity()`: Computes the similarity score using data.table.
+#'
+#' @examples
+#' # Example file path
+#' example_file <- tempfile()
+#' writeLines(c("3   4", "4   3", "2   5", "1   3", "3   9", "3   3"), example_file)
+#'
+#' # Base R Workflow
+#' base_data <- base_01_input(example_file)
+#' processed_data <- base_01_process(base_data)
+#' base_01_distance(processed_data) # Expected result: 11
+#' base_01_similarity(processed_data) # Expected result: 31
+#'
+#' # data.table Workflow
+#' dt_data <- dt_01_input(example_file)
+#' dt_01_distance(dt_data) # Expected result: 11
+#' dt_01_similarity(dt_data) # Expected result: 31
+#'
+#' unlink(example_file)
+#'
+#' @name day01
+NULL
 
+# Base R Functions -------------------------------------------------------------
+
+#' @rdname day01
+#' @export
 base_01_input <- function(x) {
-  #TODO: add type check
+  if (!file.exists(x)) stop("File does not exist.")
   read.delim(x, header = FALSE)
 }
 
-base_01_helper <- function(x) {
+#' @rdname day01
+#' @export
+base_01_process <- function(x) {
   x[, 1] |>
-    strsplit('\\s+') |>
+    strsplit("\\s+") |>
     lapply(as.numeric) |>
     do.call(rbind, args = _)
 }
 
-# solution is 1388114
+#' @rdname day01
+#' @export
 base_01_distance <- function(x) {
   (sort(x[, 1]) - sort(x[, 2])) |>
     abs() |>
     sum()
 }
 
-# solution is 23529853
+#' @rdname day01
+#' @export
 base_01_similarity <- function(x) {
-
-  nbins = max(x)
+  nbins <- max(x)
   a <- tabulate(x[, 1], nbins)
   b <- tabulate(x[, 2], nbins)
   sum(a * b * seq_len(nbins))
 }
 
+# data.table Functions ---------------------------------------------------------
+
+#' @rdname day01
+#' @import data.table
+#' @export
 dt_01_input <- function(x) fread('inst/input01.txt', header = FALSE)
 
+#' @rdname day01
+#' @export
 dt_01_distance <- function(x){
   sum(abs(sort(x[[1]]) - sort(x[[2]])))
 }
 
+#' @rdname day01
+#' @export
 dt_01_similarity <- function(x) {
   CJ(dat$V1, dat$V2)[
     V1 == V2,
@@ -45,25 +94,3 @@ dt_01_similarity <- function(x) {
   ]
 
 }
-
-input <- 'inst/input01.txt'
-(
-  dt_01_input(input) |>
-    dt_01_distance()
-) == (
-  base_01_input(input) |>
-    base_01_helper() |>
-    base_01_distance()
-)
-
-(
-  dt_01_input(input) |>
-    dt_01_similarity()
-) == (
-  base_01_input(input) |>
-    base_01_helper() |>
-    base_01_similarity()
-)
-
-
-
